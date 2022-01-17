cfa9a279d0508730108d2787b988120cb2dd497f
barsanges <barsanges@users.noreply.github.com>
2022-01-17 22:11:26 +0100

    Fix commit 2f3e12d.

--- src/ihm/controles_auxiliaires/MetaChampOrdre.cs
+++ src/ihm/controles_auxiliaires/MetaChampOrdre.cs
@@ -171,7 +171,7 @@ namespace diplo.Contrôles_auxiliaires
             {
                 Int32 largeurCourante = 20;
                 Int32 hauteurCourante = 04 - vScrollBar.Value;
-                Int32 incrementHauteur = 25;
+                Int32 incrementHauteur = 26;
                 String belligerantPrecedent = this.ChampsOrdres[0].NomBelligerant;
                 this.ChampsOrdres[0].AfficheDrapeau();
                 foreach (ChampOrdre ordre in this.ChampsOrdres)
